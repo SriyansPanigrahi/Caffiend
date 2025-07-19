834f6d62996a271593337d1ca18953a7a224fa63
SriyansPanigrahi <sriyanspanigrahi6969@gmail.com>
2025-07-20 02:33:47 +0530

    fix: fixed some err

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
 import Authentication from "./Authentication"
 import Modal from "./Modal"
-import { useAuth } from '../context/AuthContext'
 
 export default function Layout(props) {
     const { children } = props
@@ -29,12 +29,6 @@ export default function Layout(props) {
         </header>
     )
 
-    const footer = (
-        <footer>
-            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://www.smoljames.com">Smoljames</a> <br />using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br />Check out the project on <a target="_black" href="https://www.github.com/jamezmca/reactjs-full-course">GitHub</a>!</p>
-        </footer>
-    )
-
     function handleCloseModal() {
         setShowModal(false)
     }
@@ -50,7 +44,6 @@ export default function Layout(props) {
             <main>
                 {children}
             </main>
-            {footer}
         </>
     )
 }
